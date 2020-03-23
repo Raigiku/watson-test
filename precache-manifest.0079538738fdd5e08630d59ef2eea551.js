95644cc0401024b575356a50fc06ebeaa2bab85e
Diego Castillo <raigiku@hotmail.com>
2020-03-23 14:22:31 -0500

    Updates

---
+++ precache-manifest.0079538738fdd5e08630d59ef2eea551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8801bfb33bbdd5152979ebfda54aa2",
+    "url": "/watson-test/index.html"
+  },
+  {
+    "revision": "66bc0aaaf35e2ca3bbb5",
+    "url": "/watson-test/static/css/main.9a4b0535.chunk.css"
+  },
+  {
+    "revision": "1784732ca1c633891712",
+    "url": "/watson-test/static/js/2.0bf7acd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/watson-test/static/js/2.0bf7acd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66bc0aaaf35e2ca3bbb5",
+    "url": "/watson-test/static/js/main.18967e5a.chunk.js"
+  },
+  {
+    "revision": "334ae6ea05c24659b186",
+    "url": "/watson-test/static/js/runtime-main.2e4d9e67.js"
+  }
+]);
